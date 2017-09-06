620b49a255bdc53cc677a32b1b0df922d5d74e76
Łukasz Sobek <lukaszsobek@me.com>
2017-09-06 17:15:11 +0200

    - adds correct/wrong color pick logic

--- script.js
+++ script.js
@@ -16,16 +16,35 @@ selectedColorSpan.textContent = selectedColor
 // assign colors to squares
 let squares = document.getElementsByClassName("square");
 
+
+// set up message var
+let messageSpan = document.querySelector(".message")
+
+
 for(let i = 0; i < squares.length; i++) {
 	squares[i].style.backgroundColor = colors[i]
 
 	// add click logic to squares
 	squares[i].addEventListener("click", function() {
 		if(this.style.backgroundColor == selectedColor) {
-			console.log("yes")
+			
+			messageSpan.textContent = "Correct!"
+			changeColors(selectedColor)
+
 		} else {
-			console.log("nope")
+
+			this.style.backgroundColor = "#333"
+			messageSpan.textContent = "Try again!"
 		}
+
+
 	})
+}
+
+function changeColors(color) {
+	for(let i = 0; i < squares.length; i++) {
+	 	squares[i].style.backgroundColor = color;
+	}
+	
 }
 
